517b580eb363958a0d6b0517967a705412d55e0b
Adam Zhao <adamtieao@gmail.com>
2015-03-16 22:49:18 -0400

    Finished showing the data using Angularjs

---
+++ Assignment_1/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module("patientApp", []);
+
+app.controller("PatientCtrl", function ($scope, $http) {
+    
+    $http.get('Patient.json')
+    .success(function (data, status, headers, config) {
+        $scope.patients = data;
+    })
+    .error(function (data, stataus, heders, config) {
+        alert("error retrieving file");
+    });
+    $scope.orderProp = "firstName";
+});
--- Assignment_1/server.js
+++ Assignment_1/server.js
@@ -1,6 +1,14 @@
 ﻿var http = require('http');
+var express = require('express');
+
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function (request, response) {
+    response.sendfile("index.html");
+});
+
+app.listen(port);
