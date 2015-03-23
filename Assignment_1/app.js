a27ed4ec7f16de1eb93e2f072e292323facef64e
Adam Zhao <adamtieao@gmail.com>
2015-03-23 18:44:54 -0400

    Finish Assignment#1

--- Assignment_1/app.js
+++ Assignment_1/app.js
@@ -3,19 +3,26 @@
 app.config(['$routeProvider',
   function ($routeProvider) {
         $routeProvider
-        //serve up the list of customers
+        // go to PatientList.html
+        .when('/', {
+            templateUrl: 'Views/PatientList.html',
+            controller: 'PatientCtrl'
+        })
         .when('/Patients', {
             templateUrl: 'Views/PatientList.html',
             controller: 'PatientListCtrl'
         })
+        // go to PatientNew.html
         .when('/NewPatient', {
             templateUrl: 'Views/PatientNew.html',
             controller: 'PatientNewCtrl'
         })
+        // go to PatientDetails.html
         .when('/PatientDetails/:id', {
             templateUrl: 'Views/PatientDetails.html',
             controller: 'PatientDetailCtrl'
         })
+        // exception
         .otherwise({
             redirectTo: '/'
         });
--- Assignment_1/controller.js
+++ Assignment_1/controller.js
@@ -1,75 +1,116 @@
-﻿var patientControllers = angular.module("patientControllers", []);
+﻿
+var patientControllers = angular.module("patientControllers", []);
 
-patientControllers.controller("PatientListCtrl", ['$scope', '$http', function ($scope, $http) {
-    
+// a memory of patient
+var patientMem = [];
+var patientEdit;
+var index;
+var idLength;
+
+// Push the data from json file into the array patientMem[]
+patientControllers.controller("PatientCtrl", ['$scope', '$http', function ($scope, $http) {
+        
+    // get data from Patient.json file
     $http.get('Patient.json')
     .success(function (data, status, headers, config) {
-        $scope.patients = data;
-    })
+            $scope.patients = data;
+            patientMem = data;
+        })
+
+    // exception handling
     .error(function (data, stataus, heders, config) {
         alert("error retrieving file");
     });
-    $scope.orderProp = "lastName";
+        $scope.orderProp = "lastName";
+        //console.log(patientMem);
     }]);
 
+// Show the list of patients by First Name and Last Name
+patientControllers.controller("PatientListCtrl", ['$scope', '$http', function ($scope, $http) {
+        $scope.patients = patientMem;
+        $scope.orderProp = "lastName";
+        console.log(patientMem);
+    }]);
 
+    // show the details of each patient
 patientControllers.controller('PatientDetailCtrl', ['$scope', '$routeParams','$http','$location',
-  function ($scope, $routeParams, $http,$location) {
-
+  function ($scope, $routeParams, $http, $location) {
+        
+        $scope.fn = /([A-Z]{1}[a-z]*)\w+/;
+        $scope.ln = /([A-Z]{1}[a-z]*)\w+/;
+        $scope.pn = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        $scope.dt = /^([0]?[1-9]|[1][0-2])[./-]([0]?[1-9]|[1|2][0-9]|[3][0|1])[./-]([0 - 9]{ 4}|[0-9]{ 2})$/;
+        
+      // check which patient is selected
         console.log($routeParams);
 
-        var id = $routeParams.id;
+        index = $routeParams.id;
 
-        $http.get('Patient.json')
-    .success(function (data, status, headers, config) {
-            $scope.patients = data;
-            console.log(id);
-            console.log($scope.patients[id - 1]);
-            $scope.patient = $scope.patients[id - 1];
+        $scope.patients = patientMem;
+        $scope.patient = $scope.patients[index - 1];
+        patientEdit = $scope.patients[index - 1];
+
+        $scope.save = function () {
+            
+            // to check if save() is called
+            console.log('save');
+            
+            // id cannot be changed
+            $scope.patient.id = index;
+
+            // add all the data to one object
+            $scope.patient = {
+                "id": $scope.patient.id, "firstName": $scope.patient.firstName,
+                "lastName": $scope.patient.lastName, "phoneNo": $scope.patient.phoneNo,
+                "lastVisitDate": $scope.patient.lastVisitDate, "Status": $scope.patient.Status
+            };
+            
             console.log($scope.patient);
-
-        })
-    .error(function (data, stataus, heders, config) {
-            alert("error retrieving file");
-        });
+            if (index != -1) {
+                patientMem[index - 1] = patientEdit;
+                alert("Made change of the patient with ID of: " + $scope.patient.id);
+                index = -1;
+            }
+            else { 
+                alert("Please go back to select the patient to edit!");
+            }
+            
+            console.log(patientMem);
+            $scope.patient = "";
+        }       
             
     }]);
 
     
-
+    // add a new patient
     patientControllers.controller('PatientNewCtrl', ['$scope', '$routeParams',
     function ($scope, $routeParams) {
 
+        // validation
         $scope.fn = /([A-Z]{1}[a-z]*)\w+/;
         $scope.ln = /([A-Z]{1}[a-z]*)\w+/;
         $scope.pn = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        $scope.dt = /^([0]?[1-9]|[1][0-2])[./-]([0]?[1-9]|[1|2][0-9]|[3][0|1])[./-]([0 - 9]{ 4}|[0-9]{ 2})$/;
 
 
         $scope.savePatient = function () {
-            var isValid = false;
+            
+            // to check if savePatient() is called
             console.log('save');
+            
+            // add all the data to one object
             $scope.patient = {
-                "id": $scope.patient.id, "firstName": $scope.patient.firstName,
+                "id": patientMem.length + 1, "firstName": $scope.patient.firstName,
                 "lastName": $scope.patient.lastName, "phoneNo": $scope.patient.phoneNo,
-                "lastVisitDate": $scope.patient.lastVisitDate,"Status":$scope.patient.Status
+                "lastVisitDate": $scope.patient.lastVisitDate, "Status": $scope.patient.Status
             };
 
             console.log($scope.patient);
-        }
+            patientMem.push($scope.patient);
+            alert("Added a patient with ID of: " + $scope.patient.id);
+            console.log(patientMem);
+            $scope.patient = "";
+        }       
+        
 
-        
-        
-        $scope.reset = function () {
-            this.patient = {};
-        };
-        
-        $scope.reset();
-
-        ///...more logic
     }]);
-    
-
-//First Name: <span class="label label-primary">{ { patient.firstName }}</span> Last Name: <span class="label label-primary">{ { patient.lastName }}</span><br />
-//Phone Number: <span class="label label-primary">{{ patient.phoneNo }}</span><br />
-//Last Visit Date: <span class="label label-primary">{{patient.lastVisitDate}}</span><br />
-//Status: <span class="label label-primary">{{patient.Status}}</span>  {{patient.id}},{{patient.firstName}},{{patient.lastName}},{{patient.phoneNo}},{{patient.lastVisitDate}},{{patient.Status}}
